cd4bea560b3692e56192c750653e9ee65445292e
Jim M. McClellan <lyrimac@gmail.com>
2025-05-05 19:25:55 -0400

    Add timestamps to saved quotes

--- logger.py
+++ logger.py
@@ -1,1 +1,9 @@
-print("Enter your favorite quote!")
+from datetime import datetime
+
+quote = input("Enter your favorite quote: ")
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+with open("quotes.txt", "a") as file:
+    file.write(f"[{timestamp}] {quote}\n")
+
+print("Quote saved with timestamp!")
